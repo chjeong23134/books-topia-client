798bb2f1e300a97062fe20e7bccba0eb21f0f25b
chjeong23134 <chjeong23134@gmail.com>
2024-06-02 06:58:10 +0900

    design : 네비바 심플 UI 작성

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -11,12 +11,32 @@ export default function Nav() {
 	return (
 		<div className={styles.nav}>
 			<div className={styles.menuWrapper}>
+
 				<div className={styles.logo}>
 					<Image src={logo}
 						height={40}
 						width={200}
 						alt="SVG" />
+				</div>
 
+				<div className={styles.button}>
+					도서
+				</div>
+
+				<div className={styles.button}>
+					커뮤니티
+				</div>
+			</div>
+
+			<div className={styles.menuWrapper}>
+				<input className={styles.input} />
+
+				<div className={styles.button}>
+					로그인
+				</div>
+
+				<div className={styles.button}>
+					회원가입
 				</div>
 			</div>
 		</div>
